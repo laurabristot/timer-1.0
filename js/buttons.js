d6ac46474e11d82674827cc55089f75fa19e2306
laura bristot <laurabristotbarbi@hotmail.com>
2023-01-27 01:01:10 -0300

    first commit

---
+++ js/buttons.js
@@ -0,0 +1,10 @@
+export const Button = {
+   Play: document.querySelector('.play'),
+   Pause: document.querySelector('.pause'),
+   AudioOn: document.querySelector('.audioOn'),
+   AudioOff: document.querySelector('.audioOff'),
+
+   SetTimer: document.querySelector('.timer'),
+   Reset: document.querySelector('.reset'),
+
+}
---
+++ js/script.js
@@ -0,0 +1,95 @@
+import {Button} from './buttons.js'
+
+const url = 'https://www.youtube.com/watch?v=aCfVsrpMhHs'
+
+let minutes;
+let seconds;
+let timerTimeout;
+
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+function resetControls () {
+  Button.SetTimer.classList.remove('hide')
+  Button.Reset.classList.add('hide')
+  Button.Play.classList.remove('hide')
+  Button.Pause.classList.add('hide')
+  resetTimer()
+}
+
+function resetTimer(){
+  clearTimeout(timerTimeout)
+  updateTimerDisplay(25, 0)
+}
+
+function playPause (){
+  Button.Play.classList.toggle('hide')
+  Button.Pause.classList.toggle('hide')
+}
+
+function updateTimerDisplay(minutes, seconds){
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
+
+function countdown() {
+  timerTimeout = setTimeout(function(){
+    seconds = Number(secondsDisplay.textContent)
+    minutes = Number(minutesDisplay.textContent)
+
+
+    if (minutes <= 0 && seconds <= 0){
+      resetControls()
+      return
+    }
+
+    if(seconds <= 0) {
+      seconds = 10
+      --minutes
+    }
+    
+    updateTimerDisplay(minutes, String(seconds-1) )
+    
+    
+    countdown()
+    }, 1000)
+
+}
+
+
+
+Button.Play.onclick = function(){
+  playPause ()
+  Button.SetTimer.classList.toggle('hide')
+  Button.Reset.classList.toggle('hide')
+
+  countdown()
+}
+
+Button.Pause.onclick = function(){
+  playPause ()
+  clearTimeout(timerTimeout)
+}
+
+Button.Reset.onclick =  function(){
+  resetControls ()
+  clearTimeout(timerTimeout)
+}
+
+
+Button.AudioOn.onclick =  function(){
+  Button.AudioOn.classList.toggle('hide')
+  Button.AudioOff.classList.toggle('hide')
+}
+
+Button.AudioOff.onclick =  function (){
+  Button.AudioOn.classList.toggle('hide')
+  Button.AudioOff.classList.toggle('hide')
+}
+
+Button.SetTimer.onclick = function(){
+  minutes = prompt('Quantos minutos?') || 0
+  updateTimerDisplay(minutes, 0)
+}
+
+//funcao para diminuir o tempo é com setTimeOut a cada 1s --segundo e a cada 60s --minuto
